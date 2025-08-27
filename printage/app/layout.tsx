3651356b327fb78242ed504dedbc0d05cf7cd616
Feyitee <anjuwontobi99@gmail.com>
2025-08-27 03:40:03 +0100

    dynamic routes

--- printage/app/layout.tsx
+++ printage/app/layout.tsx
@@ -1,8 +1,8 @@
 import "./globals.css";
+import Link from "next/link";
 import { Albert_Sans, Montserrat_Alternates } from "next/font/google";
 import PFLogoIcon from "@/public/printforge-logo-icon.svg";
 import PFLogo from "@/public/printforge-logo.svg";
-import Image from "next/image";
 
 const albertSans = Albert_Sans({
   subsets: ["latin"],
@@ -15,6 +15,7 @@ const montserratAlternates = Montserrat_Alternates({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   variable: "--font-montserrat-alternates",
 });
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -27,23 +28,29 @@ export default function RootLayout({
       >
         <header className="w-full bg-white">
           <nav className="flex justify-between px-6 py-4">
-            <div className="relative">
-              {/* Desktop logo */}
-              <Image
-                src={PFLogo}
-                alt="PrintForge Logo"
-                className="w-[200px] h-auto hidden md:block"
-              />
-              {/* Mobile logo */}
-              <Image
-                src={PFLogoIcon}
-                alt="PrintForge Logo"
-                className="w-[40px] h-auto block md:hidden"
-              />
-            </div>
+            <Link href="./about">
+              <div className="relative cursor-pointer">
+                {/* Desktop Logo */}
+                <img
+                  src={PFLogo.src}
+                  alt="PrintForge Logo"
+                  className="w-[200px] h-auto hidden md:block"
+                />
+                {/* Mobile Logo */}
+                <img
+                  src={PFLogoIcon.src}
+                  alt="PrintForge Logo"
+                  className="w-[40px] h-auto block md:hidden"
+                />
+              </div>
+            </Link>
             <ul className="flex items-center gap-2.5">
-              <p>3D Models</p>
-              <p>About</p>
+              <li className="text-sm uppercase cursor-pointer">
+                <Link href="/3d-models">3D Models</Link>
+              </li>
+              <li className="text-sm uppercase cursor-pointer">
+                <Link href="/about">About</Link>
+              </li>
             </ul>
           </nav>
         </header>
